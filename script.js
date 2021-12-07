ba914e38eb27f07b375d2602b599db3928827337
STLPhilly <izachariahh@gmail.com>
2021-12-07 12:06:27 -0600

    Complete.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-// Write your JavaScript code here!
+
 
 window.addEventListener("load", function() {
+    document.addEventListener("submit", function(event) {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+        event.preventDefault()
+        let pilot = document.querySelector("input[name=pilotName]");
+        let copilot = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoLevel = document.querySelector("input[name=cargoMass]");
+
+        //function call
+        formSubmission(document, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value)
+    });
+
+
+
+// This is taking our myFetch function and inserting it into our placeholder listedPlanetResponse that is turning our function and json into a variable for use in our second function pickPlanet which will randomly generate a number.
+    let listedPlanets;
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+
+        let planet = listedPlanets[pickPlanet()]
+
+        // The listedPlanets that was fetched is an array of 6 objects. Therefore our random number will be our index to choose which planet to display.
+        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
+    })
+
 });
+
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,40 +1,95 @@
-// Write your helper functions here!
 require('isomorphic-fetch');
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
-
+// This function will alert if the form is empty, and return values for if our placeholder 'testInput' is a number or not.
 function validateInput(testInput) {
-   
+    if (testInput == ""){
+        alert("All fields are required");
+    }
+    else if (isNaN(testInput)){
+        return "Not a number";
+    }
+    else{
+        return "Is a number";
+    };
+};
+
+// This function will change our CSS and HTML based on fuelLevel and cargoLevel, it will also alert based on isNan or String.
+function formSubmission(document, pilot, copilot, fuelLevel, cargoLevel) {
+    
+
+    if (validateInput(pilot) == "Is a number" || validateInput(copilot) == "Is a number"){
+        alert("Error: Invalid name(s).")
+    }
+    else if (validateInput(fuelLevel) == "Not a number" || validateInput(cargoLevel) == "Not a number"){
+        alert("Error: invalid value(s).")
+    }
+
+    // This function will change the CSS data based on our form.
+
+    // Here we change the status of our pilots to be ready.
+    document.getElementById("pilotStatus").innerHTML = `${pilot} is Ready.` 
+    document.getElementById("copilotStatus").innerHTML = `${copilot} is Ready.` 
+
+    // In our if statements we change status to failing status.
+    if(fuelLevel < 10000){
+        document.getElementById("fuelStatus").innerHTML = "There is not enough fuel for the journey."
+    }
+
+    if(cargoLevel > 10000){
+        document.getElementById("cargoStatus").innerHTML = "There is too much mass for the shuttle to take off."
+    }
+
+    if(fuelLevel < 10000 || cargoLevel > 10000){
+        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch."
+        document.getElementById("launchStatus").style.color = "red";
+        document.getElementById("faultyItems").style.visibility = "visible";
+    }
+    
+    // But in our else statement, we are saying that everything is passing and good to go.
+    else{
+        document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch."
+        document.getElementById("launchStatus").style.color = "green";
+        document.getElementById("faultyItems").style.visibility = "visible";
+    }
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
 
+// async is in reference to HTTP Asynchronous meaning that it's waiting for data.
+// This function is FETCHING the .json and places the array of objects into a variable.
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json();
         });
 
     return planetsReturned;
 }
 
-function pickPlanet(planets) {
+// This function is randomizing the planets after the data is converted into Javascript friendly variables initialized in script.js 
+function pickPlanet() {
+    let num = Math.random(4*10)*6
+    let planet = Math.floor(num)
+    console.log(planet)
+    return planet;
 }
+
+// This function is inserting our HTML into our index.html file, using template literals to insert the information gathered through our listedPlanets, that was gathered with myFetch(), and selected with pickPlanet().
+function addDestinationInfo(document, name, diameter, star, distance, moons, image) {
+    document.getElementById("missionTarget").innerHTML =
+                `<h2>Mission Destination</h2>
+                <ol>
+                    <li>Name: ${name} </li>
+                    <li>Diameter: ${diameter} </li>
+                    <li>Star: ${star}</li>
+                    <li>Distance from Earth: ${distance} </li>
+                    <li>Number of Moons: ${moons} </li>
+                </ol>
+                <img src="${image}">`
+
+}
+
+
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
